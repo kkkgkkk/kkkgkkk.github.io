2b3339896b892a6cb5c0871f41a9d6339b3562ab
kkkgkkk <155944490+kkkgkkk@users.noreply.github.com>
2024-01-17 20:37:05 +0800

    Add files via upload
    Signed-off-by: kkkgkkk <155944490+kkkgkkk@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
+
+// 取得具有 id 為 "header-content" 的元素
 let tar = document.getElementById("header-content");
+
+// 取得具有 id 為 "hamburger" 的元素
 let ham = document.getElementById("hamburger");
 
+// 為 "hamburger" 元素添加點擊事件監聽器
 ham.addEventListener("click", function (event) {
+  // 切換 "header-content" 元素的 "header-content_active" class
   tar.classList.toggle("header-content_active");
 });
